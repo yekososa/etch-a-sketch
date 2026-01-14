9a80758ffb1b943f80180441bc7802156a568bff
zeuzu <yekososa@gmail.com>
2026-01-13 21:00:35 -0800

    change hover functionality and stretch of cells

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+function createGrid(val) {
+  const container = document.querySelector(".container");
+  container.replaceChildren();
+
+  for (let i = 0; i < val; i++) {
     let row = document.createElement("div");
-    row.classList.add("flexcontainer")
+    row.classList.add("flexcontainer");
 
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div");
-        cell.textContent = "cell";
+    for (let j = 0; j < val; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
 
-        cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = "red";
-        })
+      cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "purple";
+      });
 
-        cell.addEventListener("mouseleave", () => {
-            cell.style.backgroundColor = "purple";
-        })
+    //   cell.addEventListener("mouseleave", () => {
+    //     cell.style.backgroundColor = "white";
+    //   });
 
-        row.appendChild(cell);
+      row.appendChild(cell);
     }
     container.appendChild(row);
+  }
 }
+const button = document.querySelector("button");
 
+button.addEventListener("click", () => {
+  const val1 = prompt("Enter number of squares per side for new grid");
+
+  if (val1 > 100) {
+    alert("Entered value must be less than or equal to 100");
+  } else {
+    createGrid(val1);
+  }
+});
+
+createGrid(16);
